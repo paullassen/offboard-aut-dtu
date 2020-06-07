b742181598417bc00a615145189a8e302e93179e
Paul Lassen <s182864@student.dtu.dk>
2020-06-07 21:01:30 +0200

    Increased max thrust

--- src/uav_monitor.cpp
+++ src/uav_monitor.cpp
@@ -213,7 +213,7 @@ float UavMonitor::calculate_thrust(){
 	double scale = cos((double) roll * M_PI/180)*cos((double) pitch * M_PI/180);
 
 	uav_thrust = thrust / abs((float)scale);
-	return saturate_minmax(uav_thrust, 0, 0.22);
+	return saturate_minmax(uav_thrust, 0, 0.25);
 }
 
 float UavMonitor::calculate_pitch(){
